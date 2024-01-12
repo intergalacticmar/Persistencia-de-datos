e9375aabf77136853d440af6baffb6a49ac8fea5
María <120553496+intergalacticmar@users.noreply.github.com>
2024-01-12 11:37:42 +0100

    guardado 1

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveManager : MonoBehaviour
+{
+    [SerializeField] private Text checkPointText; 
+    [SerializeField] private Text userPositionText;
+    [SerializeField] private string checkPoint;
+    [SerializeField] private Vector3 userPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadData();
+    }
+
+    public void SaveData()
+    {
+        PlayerPrefs.SetString("CheckPoint", checkPoint);
+        PlayerPrefs.SetFloat("positionX", userPosition.x);
+        PlayerPrefs.SetFloat("positionY", userPosition.y);
+        PlayerPrefs.SetFloat("positionZ", userPosition.z);
+
+        LoadData();
+    }
+
+    void LoadData()
+    {
+        checkPointText.text = "CheckPoint: " + PlayerPrefs.GetString("CheckPoint", "No pound found");
+        userPositionText.text = "Player position: " + PlayerPrefs.GetFloat("positionX", 0).ToString() + "x" + PlayerPrefs.GetFloat("positionY", 0).ToString() + "y" + PlayerPrefs.GetFloat("positionZ", 0).ToString() + "z";
+    }
+
+}
+
